684b4c903094dfaf6f316084a86265ea380b6eeb
JohnFordBR <nice.knyazyan@bk.ru>
2017-07-17 07:17:09 +0400

    malware detection

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 'use strict';
+
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
-
+let mainWindow= null;
 app.on('ready', ()=>{
-   const mainWindow = new BrowserWindow({
-        height: 600,
-        width: 600
+    mainWindow = new BrowserWindow({
+        height:600,
+        width:1200
     });
-  mainWindow.loadURL('file://' + __dirname + 'index.html');
+   mainWindow.loadURL('file://' + __dirname + '/index.html');
 });
