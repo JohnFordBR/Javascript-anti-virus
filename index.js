c475e1550755e0021c7664a66427dcf7d2a00129
JohnFordBR <nice.knyazyan@bk.ru>
2017-07-26 23:26:01 +0400

    design stuff

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const BrowserWindow = electron.BrowserWindow;
 let mainWindow= null;
 app.on('ready', ()=>{
     mainWindow = new BrowserWindow({
-        height:600,
+        height:625,
         width:1200
     });
    mainWindow.loadURL('file://' + __dirname + '/index.html');
